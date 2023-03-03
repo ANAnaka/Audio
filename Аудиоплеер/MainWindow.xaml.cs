a3bcfa4b15f8096f4d0a78654712da2b27fa401f
ANAnaka <112645412+ANAnaka@users.noreply.github.com>
2023-03-03 21:52:36 +0300

    Add files via upload

---
+++ Аудиоплеер/MainWindow.xaml.cs
@@ -0,0 +1,82 @@
+﻿using Microsoft.WindowsAPICodePack.Dialogs;
+using System;
+using System.Collections.Generic;
+using System.Collections.ObjectModel;
+using System.IO;
+using System.Linq;
+using System.Text;
+using System.Text.RegularExpressions;
+using System.Threading.Tasks;
+using System.Windows;
+using System.Windows.Controls;
+using System.Windows.Data;
+using System.Windows.Documents;
+using System.Windows.Forms;
+using System.Windows.Input;
+using System.Windows.Media;
+using System.Windows.Media.Imaging;
+using System.Windows.Navigation;
+using System.Windows.Shapes;
+
+
+namespace Аудиоплеер
+{
+    
+    public partial class MainWindow : Window
+    {
+        public static List<string> list = new List<string>();
+        private MediaPlayer player = new MediaPlayer();
+        public MainWindow()
+        {
+            InitializeComponent();
+
+            List<string> list = new List<string>();
+        }
+        
+        public void Papka_Click(object sender, RoutedEventArgs e)
+        {
+            CommonOpenFileDialog dlg = new CommonOpenFileDialog() { IsFolderPicker = true};
+            CommonFileDialogResult result = dlg.ShowDialog();
+            if (result == CommonFileDialogResult.Ok)
+            {
+                string[] files = Directory.GetFiles(dlg.FileName);
+                Regex regex = new Regex(@"\w*\.mp3|\w*\.m4a|\w*\.wav");
+                foreach (string item in files)
+                {
+                    if (regex.IsMatch(item))
+                    {
+                        MusVbr.Items.Add(regex.Match(item).Value);
+                        list.Add(item);
+
+                    }
+                }
+            }
+        }
+
+        private void Media_MediaOpened(object sender, RoutedEventArgs e)
+        {
+            LengthSlider.Maximum = Media.NaturalDuration.TimeSpan.Ticks;
+        }
+
+        private void AudioSlider_ValueChanged(object sender, RoutedPropertyChangedEventArgs<double> e)
+        {
+            Media.Position = new TimeSpan(Convert.ToInt64(AudioSlider.Value));
+        }
+
+        private void MusVbr_SelectionChanged(object sender, SelectionChangedEventArgs e)
+                {
+                    player = MusVbr.SelectedItem as MediaPlayer;
+                }
+
+        private void PlayBtn_Click(object sender, RoutedEventArgs e)
+        {
+
+            //player.Open();
+            //player.Play();
+
+        }
+
+        
+    }
+}
+
